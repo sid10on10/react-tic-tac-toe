54d7dd5c53cf567e1833c638fe258ce228eb8f3f
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-10-11 12:13:54 +0530

    first commit

---
+++ src/Components/Game.js
@@ -0,0 +1,39 @@
+import React,{useState} from "react"
+import Board from "./Board"
+import { calculateWinner } from "./helpers"
+
+
+const Game = () => {
+
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xIsNext, setXisNext] = useState(true);
+    const winner = calculateWinner(board);
+
+    const handleClick = (i) => {
+        const boardCopy = [...board];
+        // If user click an occupied square or if game is won, return
+        if (winner || boardCopy[i]) return;
+        // Put an X or an O in the clicked square
+        boardCopy[i] = xIsNext ? "X" : "O";
+        setBoard(boardCopy);
+        setXisNext(!xIsNext);
+    };
+
+    const renderMoves = () => (
+        <button className="btn-primary ml-3" onClick={()=>setBoard(Array(9).fill(null))}>
+            Reset Game
+        </button>
+    )
+
+    return (
+        <>
+        <Board squares={board} onClick={handleClick}/>
+        <div className="result">
+            <p>{winner ? "Winner "+winner : "Next Player : "+ (xIsNext?"X":"O") }</p>
+            {renderMoves()}
+        </div>
+        </>
+    )
+}
+
+export default Game;
